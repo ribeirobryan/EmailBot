56e8be280c3b08320e09fee4413a6d19694591f5
Bryan Ribeiro <ribeirobryan@gmail.com>
2023-10-19 20:12:25 +0100

    Add files via upload

--- script.js
+++ script.js
@@ -1,17 +1,23 @@
 
 function sendEmail(){
     
-    let toaddrs = document.getElementById("toaddrs").value
-    let messageSubject = document.getElementById("messageSubject").value
-    let messageContent = document.getElementById("messageContent").value
-    
+    let userEmailAddress = document.getElementById("userEmailaddress").value
+    let userSubject = document.getElementById("subject").value
+    let userMessage = document.getElementById("messageContent").value
+    let userName = document.getElementById("userName").value
+    let userPhoneNumber = document.getElementById("userPhoneNumber").value
+
+    let messageSubject = userSubject + " - " + userName
+
+    let messageContent = userMessage + "        " + userPhoneNumber
+
 
     Email.send({
         
         Host : "smtp.elasticemail.com",
         Username : "ribeirobryandois@gmail.com",
         Password : "670DFB5650448756C7ED1BFBC03E853084CE",
-        To : toaddrs,
+        To : "ribeirobryandois@gmail.com",
         From : "ribeirobryandois@gmail.com",
         Subject : messageSubject,
         Body : messageContent
